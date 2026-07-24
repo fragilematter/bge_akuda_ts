4292236c9e8ba8fa42974867fc486c0b38853eba
Doru Barbu <barbu.doru@gmail.com>
2026-07-24 11:29:54 +0300

    Added copyright header

--- src/decoder.ts
+++ src/decoder.ts
@@ -1,3 +1,21 @@
+/*      bge_akuda_ts Beyond Good & Evil Akuda Bar Locker Decoder - typescript rewrite of JDarkroom
+ *      Copyright (C) 2023 TheEntropyShard
+ *      Copyright (C) 2026 fragilematter
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const CODE_OFFSET = 0x00002D58;
 const KEY = new Uint8Array([0x25, 0x1F, 0x1D, 0x17, 0x13, 0x11, 0x0B, 0x07]);
 
